254c5412c1c80e2ced57fb14eee59283acdffbb6
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-11-24 20:28:44 -0800

    chore: finish

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-$(function() {
-  // YOUR CODE HERE
-})
+$(function () {
+  // exercise 1
+  $("#toggleButton").click(function () {
+    $("#myParagraph").toggle();
+  });
+
+  // exercise 2
+  $("#colorButton").click(function () {
+    $("#colorDiv").css("background-color", "yellow");
+  });
+
+  // exercise 3
+  $("#addClassButton").click(function () {
+    $("#classDiv").addClass("newClass");
+  });
+  $("#removeClassButton").click(function () {
+    $("#classDiv").removeClass("newClass");
+  });
+
+  // exercise 4
+  $("#fadeInButton").click(function () {
+    $("#fadeDiv").fadeIn();
+  });
+  $("#fadeOutButton").click(function () {
+    $("#fadeDiv").fadeOut();
+  });
+
+  // exercise 5
+  $("#slideUpButton").click(function () {
+    $("#slideDiv").slideUp();
+  });
+  $("#slideDownButton").click(function () {
+    $("#slideDiv").slideDown();
+  });
+});
